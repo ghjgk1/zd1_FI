30b4af721eb7b945e537d59efb749bbbc910adbf
RyzhkovAA <qwerty@bebr.ru>
2024-11-06 14:19:05 +0500

    Добавьте файлы проекта.

---
+++ Cat.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cat
+{
+    //Создадим класс “Кот”
+    internal class Cat
+    {
+        //скрытое поле имя
+        private string name;
+
+        //свойство, для вззаимодействия с полем имя
+        public string Name
+        {
+            // получение значения
+            get { return name; }
+            // установка значения
+            set
+            {
+                bool OnlyLetters = true;
+                // ключ. слово value - это то, что хотят свойству присвоить
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                //условие проверяет на то что в перемнной буквы и она не пустая
+                if (OnlyLetters && value != "")
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                }
+
+            }
+        }
+
+        //Конструктор для создания нового экземпляра
+        public Cat(string CatName)
+        {
+            Name = CatName;
+        }
+
+        public Cat()
+        {
+        }
+
+        //Метод, который бы позволял опознать отдельно взятого кота
+        public void Meow()
+        {
+            Console.WriteLine($"{name}: МЯЯЯЯУ!!!!");
+        }
+
+        //скрытое поле вес
+        private string ves;
+
+        // свойство, для вззаимодействия с полем вес
+        public string Ves
+        {
+            get
+            {
+                return ves;
+            }
+            set
+            {
+                //проверка на некоректность ввода и число не бесконечнность и 0<x!=0
+                if (Convert.ToDouble(value) > 0)
+                {
+                    ves = value;
+                }
+                else
+                {
+                    Console.WriteLine("Weight is not correct");
+                }
+            }
+        }
+
+
+        public void CName(string CatName)
+        {
+            Name = CatName;
+        }
+
+        public void CatInfo()
+        {
+            Console.WriteLine($"{name}-{ves}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Создаем первого кота
+                Cat Cat1 = new Cat();
+
+                //Создаем второго кота
+                Cat Cat2 = new Cat();
+
+                //Добавляем имя первого кота
+                do
+                {
+                    Console.WriteLine("Введите имя первого кота");
+                    Cat1.Name = Console.ReadLine();
+                }
+                while (Cat1.Name == null);
+
+                //Добавляем имя второго кота
+                do
+                {
+                    Console.WriteLine("Введите имя второго кота");
+                    Cat2.Name = Console.ReadLine();
+                }
+                while (Cat2.Name == null);
+
+                //Добавляем вес первого кота
+                Console.WriteLine("Введите вес первого кота");
+                Cat1.Ves = Console.ReadLine();
+
+                //Добавляем вес второго кота
+                Console.WriteLine("Введите вес второго кота");
+                Cat2.Ves = Console.ReadLine();
+
+
+                //Мяукаем первым котом
+                Cat1.Meow();
+
+                //Мяукаем вторым котом
+                Cat2.Meow();
+
+                //Выводим информацию о первом коте
+                Cat1.CatInfo();
+
+                //Выводим информацию о втором коте
+                Cat2.CatInfo();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            Console.ReadKey();
+        }
+       
+    }
+}
